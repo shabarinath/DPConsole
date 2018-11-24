207b680f649ada680cf31f0058b6dd2aa72165b7
nanda.malve <nanda.malve@opsramp.com>
2018-11-24 18:42:00 +0530

    Parsers Skeleton

---
+++ src/main/java/com/dpconsole/parsers/FoodPandaParser.java
@@ -0,0 +1,29 @@
+/*
+ * This computer program is the confidential information and proprietary trade
+ * secret of OpsRamp, Inc. Possessions and use of this program must
+ * conform strictly to the license agreement between the user and
+ * OpsRamp, Inc., and receipt or possession does not convey any rights
+ * to divulge, reproduce, or allow others to use this program without specific
+ * written authorization of OpsRamp, Inc.
+ *
+ * Copyright 2018 OpsRamp, Inc. All Rights Reserved.
+ */
+package com.dpconsole.parsers;
+
+import java.util.List;
+
+import com.dpconsole.model.order.Order;
+
+/**
+ * @author nanda.malve
+ * created on 24-Nov-2018 6:24:45 PM
+ */
+public class FoodPandaParser implements Parser {
+
+	@Override
+	public List<Order> parse(Object content) throws Exception {
+		// TODO
+		return null;
+	}
+
+}
--- src/main/java/com/dpconsole/parsers/Parser.java
+++ src/main/java/com/dpconsole/parsers/Parser.java
@@ -1,17 +1,15 @@
 package com.dpconsole.parsers;
 
-import javax.mail.Message;
+import java.util.List;
 
 import com.dpconsole.model.order.Order;
 
 /**
  * @author SHABARINATH
- * 23-Nov-2018 5:48:14 pm 2018 
+ * 23-Nov-2018 5:48:14 pm 2018
  */
-
 public interface Parser {
-	Order parse(String content) throws Exception;
-	
-	Order parse(Message messages) throws Exception;
-}
 
+	List<Order> parse(Object content) throws Exception;
+
+}
