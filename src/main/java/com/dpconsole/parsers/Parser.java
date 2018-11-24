735e0788effc13ecaac69a24c84de977707222c9
shabarinath <volamshabarinath@gmail.com>
2018-11-24 23:19:18 +0530

    Changed input to Type generic

--- src/main/java/com/dpconsole/parsers/Parser.java
+++ src/main/java/com/dpconsole/parsers/Parser.java
@@ -8,8 +8,8 @@ import com.dpconsole.model.order.Order;
  * @author SHABARINATH
  * 23-Nov-2018 5:48:14 pm 2018
  */
-public interface Parser {
+public interface Parser<T> {
 
-	List<Order> parse(Object content) throws Exception;
+	List<Order> parse(T content) throws Exception;
 
 }
