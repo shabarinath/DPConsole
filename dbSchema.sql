1eb2264869288073f2ab3bc6d199123280e8930b
shabarinath <volamshabarinath@gmail.com>
2018-11-27 22:01:18 +0530

    db schema commit

--- dbSchema.sql
+++ dbSchema.sql
@@ -68,6 +68,139 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+DROP TABLE IF EXISTS `catalogue_categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `catalogue_categories` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `precedence` int(100) NOT NULL,  
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `catalogue_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `catalogue_items` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `description` text,
+  `sub_category_id` bigint(20) NOT NULL,  
+  `food_type` varchar(255) NOT NULL,
+  `precedence` int(100) NOT NULL,  
+   item_id bigint(20) NOT NULL,
+   active tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `catalogue_departments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `catalogue_departments` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `category_id` bigint(20) NOT NULL,    
+  `precedence` int(100) NOT NULL,    
+   active tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `kitchens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kitchens` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL,
+  `mailbox_username` varchar(255) NOT NULL,    
+  `mailbox_password` varchar(255) NOT NULL, 
+  `kitchen_id` bigint(20) NOT NULL,      
+   active tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `kitchen_delivery_partners`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kitchen_delivery_partners` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+  `delivery_partner` varchar(255) NOT NULL,
+  `email_ids` varchar(255) NOT NULL,
+  `commission_percentage` DOUBLE(10,4) NOT NULL,  
+   active tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `kitchen_discounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kitchen_discounts` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+  `kitchen_id` bigint(20) NOT NULL,   
+  `delivery_partner` varchar(255) NOT NULL,
+  `start_time` timestamp, 
+  `end_time` timestamp, 
+  `discount` DOUBLE(10,4) NOT NULL, 
+   active tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `kitchen_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kitchen_items` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+   item_id bigint(20) NOT NULL,
+  `kitchen_id` bigint(20) NOT NULL,   
+  `price` DOUBLE(10,4) NOT NULL,   
+   active tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `orders` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+   item_id bigint(20) NOT NULL,
+   `kitchen_id` bigint(20) NOT NULL,   
+   `delivery_partner` varchar(255) NOT NULL,
+   dp_order_id varchar(255) NOT NULL,
+   `parsed_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+   `ordered_time` timestamp, 
+   `status` varchar(255) NOT NULL,
+   `total_cost` DOUBLE(10,4) NOT NULL,  
+   `notes` DOUBLE(10,4) ,
+    manual_review tinyint(1) NOT NULL DEFAULT '0',
+	`payment_type` varchar(255) NOT NULL,  
+	manual_review_comments text DEFAULT NULL,
+    active tinyint(1) NOT NULL DEFAULT '1',  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `order_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,  
+  `version` bigint(20) NOT NULL DEFAULT '0',
+   item_id bigint(20) NOT NULL,
+  `quantity` bigint(20) NOT NULL,   
+  `unit_price` DOUBLE(10,4) NOT NULL,   
+   active tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+
 --
 -- Dumping data for table `users`
 --
